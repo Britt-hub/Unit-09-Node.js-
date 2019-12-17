2a5f35ffb2ddc007c10a9b10b9c3d6517d57991f
Brittany <britloy91@gmail.com>
2019-12-17 15:59:47 -0500

    html file

---
+++ github.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function generateHtml(data, color) {
+const htmlTemplate = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1 style = 'color: ${color}'>${data.name}</h1>
+
+    <img src = "${data.avatar_url}">
+
+    <div>
+        
+                <p>${data.login}</p>
+                <p>${data.followers}</p>
+                <p>${data.following}</p>
+                <p>${data.repos_url}</p>
+                <p>${data.bio}</p>
+                <p>${data.location}</p>
+                <p>${data.avatar_url}</p>
+    
+    </div>
+
+    
+
+</body>
+</html>`
+return htmlTemplate
+}
+
+module.exports = generateHtml;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var inquirer = require('inquirer');
-var axios = require('axios');
+const inquirer = require('inquirer');
+const axios = require('axios');
+const generateHtml = require('./github')
+const fs = require('fs');
+
 
 inquirer
     .prompt([
@@ -25,14 +28,30 @@ inquirer
         console.log(data.gitHubName)
         console.log(data.colors)
 
+
         axios.get('https://api.github.com/users/'+ data.gitHubName)
         .then(function(response){
             console.log(response.data)
+            console.log(response.data.login)
+            console.log(response.data.followers)
+            console.log(response.data.following)
+            console.log(response.data.repos_url)
+            console.log(response.data.bio)
+            console.log(response.data.location)
+            console.log(response.data.avatar_url)
+            const completedHtml = generateHtml(response.data, data.colors)
+        
             
+            fs.writeFile('./newPage.html', completedHtml, function(err){
+                if (err) {
+                    console.log(err);
+                }
+            });
 
         })
 
     });
+
 
 
     
