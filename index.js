bf420bcf7ab5670104307c067d8d64143f4a2a5d
Brittany <britloy91@gmail.com>
2019-12-17 14:29:18 -0500

    github link

---
+++ index.js
@@ -0,0 +1,38 @@
+var inquirer = require('inquirer');
+var axios = require('axios');
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your user name?",
+            name: "gitHubName"
+        },
+        {
+
+            type: "list",
+            message: "What is your favorite color",
+            name: "colors",
+            choices: [
+                "green",
+                "red",
+                "blue",
+                "yellow"
+            ]
+        }
+    ]).then(function(data) {
+        console.log(data.gitHubName)
+        console.log(data.colors)
+
+        axios.get('https://api.github.com/users/'+ data.gitHubName)
+        .then(function(response){
+            console.log(response.data)
+            
+
+        })
+
+    });
+
+
+    
+
